c359a477286086098148923c44c834ed8b43668c
Christopher Sobey <sobey.christopher@gmail.com>
2025-06-19 17:04:07 -0400

    +Added sidebar as icons -Adjusted dark colors

--- chat/src/react/components/sidebar/index.tsx
+++ chat/src/react/components/sidebar/index.tsx
@@ -1,19 +1,36 @@
+import { useMemo } from 'react';
+import styles from './styles.module.scss';
+
+const LINKS = [
+  { color: '#f00', icon: 'bank' },
+  { color: '#0f0', icon: 'building' },
+  { color: '#4040ff', icon: 'car-side' },
+  { color: '#ff0', icon: 'home' },
+  { color: '#f0f', icon: 'plane-alt' },
+  { color: '#0ff', icon: 'rocket-lunch' },
+  { color: '#f00', icon: 'truck-side' },
+];
+
+const Link = ({ color, icon, index }) => {
+  const style = {
+    '--color': color,
+    '--icon-image': `url(/icons/${icon}.svg)`,
+  };
+
+  return <a className={styles.link} href={`#chat-${index}`} style={style}>
+    Chat &#35;{index}
+  </a>;
+};
+
 export const Sidebar = () => {
-  return <aside>
-    <header>Test-Chat</header>
 
+  const links = useMemo(() => (
+    LINKS.map((entry, index) => <Link {...entry} key={index} index={index} />)
+  ), []);
+
+  return <aside className={styles.wrapper}>
     <nav>
-      <a href='#chat-1'>
-        Chat 1
-      </a>
-
-      <a href='#chat-2'>
-        Chat 2
-      </a>
-
-      <a href='#chat-3'>
-        Chat 3
-      </a>
+      {links.map(child => child)}
     </nav>
   </aside>;
 };
