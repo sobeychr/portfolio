15b936c1a2f55efe54ed2c2b6efa2c837901a2eb
Christopher Sobey <sobey.christopher@gmail.com>
2025-06-27 15:14:20 -0400

    *Corrected ChatDate React.key

--- chat/src/react/components/chat/list.tsx
+++ chat/src/react/components/chat/list.tsx
@@ -5,23 +5,27 @@ import { MessageContext } from '@context/message';
 import styles from './styles-list.module.scss';
 
 type ChatEntryParam = {
+  prevDate: number;
   message: CMessage;
 };
 
-const ChatDate = ({ date }) => {
-  return <div className={styles['date-split']}>{date.toISOString().split('T')?.[0]}</div>;
-};
+const ChatDate = ({ date }) => <div className={styles['date-split']}>{date.toISOString().split('T')?.[0]}</div>;
 
-const ChatEntry = ({ message }: ChatEntryParam) => {
-  return <article className={styles.entry}>
-    <header className={styles['entry-header']}>
-      <span className={styles['entry-time']} data-title={message.dateStr}>{message.timeStr}</span>
-      <span className={styles['entry-name']}>{message.username}</span>
-    </header>
-    <p className={styles['entry-content']}>
-      {message.content}
-    </p>
-  </article>;
+const ChatEntry = ({ message, prevDate }: ChatEntryParam) => {
+  const currentDate = message.date.getDate();
+  const isSame = prevDate === currentDate;
+
+  return (<>
+    {!isSame && <ChatDate date={message.date} />}
+    <article className={styles.entry}>
+      <header className={styles['entry-header']}>
+        <span className={styles['entry-time']} data-title={message.dateStr}>{message.timeStr}</span>
+        <span className={styles['entry-name']}>{message.username}</span>
+      </header>
+      <p className={styles['entry-content']}>
+        {message.content}
+      </p>
+    </article></>);
 };
 
 export const ChatList = () => {
@@ -35,13 +39,8 @@ export const ChatList = () => {
     return list.map((entry, index) => {
       const prev = list[index - 1];
       const prevDate = prev?.date.getDate() || 0;
-      const currentDate = entry.date.getDate();
-      const isSame = prevDate === currentDate;
 
-      return (<>
-        {isSame ? '' : <ChatDate key={prevDate} date={entry.date} />}
-        <ChatEntry key={entry.key} message={entry} />
-      </>);
+      return <ChatEntry key={entry.key} message={entry} prevDate={prevDate} />;
     });
   }, [currentChat, list.length]);
 
