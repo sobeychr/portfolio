84d96889fb8fc5b4dcdbd484a131ad3b6edd771f
Christopher Sobey <sobey.christopher@gmail.com>
2026-02-01 15:53:45 -0500

    +Added blank mock-framework

---
+++ mock-framework/blank/index.ts
@@ -0,0 +1,51 @@
+import { serve, type BunRequest, type Server } from 'bun';
+
+const PREFIX = '/blank';
+
+const mainServer = serve({
+  port: process.env.SERVER_PORT,
+  hostname: process.env.SERVER_HOST,
+
+  routes: {
+    [PREFIX + '/']: () => Response.json({
+      data: {
+        message: 'ok',
+      },
+      error: null,
+      meta: {
+        path: '/',
+        service: 'blank',
+      },
+    }),
+
+    [PREFIX + '/alpha']: () => Response.json({
+      data: {
+        message: 'alpha',
+      },
+      error: null,
+      meta: {
+        path: '/alpha',
+        service: 'blank',
+      },
+    }),
+  },
+
+  fetch: (request: BunRequest, server: Server<BunRequest>) => {
+    const requestPath = request.url
+      .replace(PREFIX, '')
+      .replace(server.url.origin, '');
+
+    return Response.json({
+      data: {
+        message: 'not found',
+      },
+      error: 'request not found',
+      meta: {
+        path: requestPath,
+        service: 'blank',
+      },
+    });
+  }
+});
+
+console.log('server running at', mainServer.url.origin.concat(PREFIX));
